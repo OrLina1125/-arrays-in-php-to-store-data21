0381611cd3efff36bcb9d064f0f4738909045761
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-05-07 11:35:39 +0900

    add array 21

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Loop 17</title>
+</head>
+<body>
+    <?php
+    //regular array
+    $array = array("Daniel", "Jane", "Jacob","John", "Mariane");
+    //$data1 = "Daniel";
+    //$data2 = "Jane";
+    //$data3 = "Jacob";
+    //$data4 = "John";
+    //$data5 = "Mariane";
+    echo $array[2];
+    ?>
+</body>
+</html>
